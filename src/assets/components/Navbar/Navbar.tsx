73e75d8055fdd434c71d55faf7a28e52d52a63c2
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-24 01:01:11 +0800

    updated navbar componpent

--- src/assets/components/Navbar/Navbar.tsx
+++ src/assets/components/Navbar/Navbar.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
+import logo from './../../images/logo.svg';
 
-// interface NavbarProps {
-   
-// }
+
 
 const Navbar: React.FC = () => {
    return (
       <>
          <div className={styles.navbar}>
-            <div className={styles.navbar__brand}></div>
+            <div className={styles.navbar__wrapper}>
+               <div className={styles.navbar__brand}>
+                  <a href="#">
+                     <img src={logo} alt="ClickMate Logo" />
+                  </a>
+               </div>
+               <div className={styles.navbar__menu}>
+                  <ul className={styles.navbar__list}>
+                     <li className={styles.navbar_item}>
+                        <a href="#">HOME</a>
+                     </li>
+                     <li className={styles.navbar_item}>
+                        <a href="#">CAMERAS</a>
+                     </li>
+                     <li className={styles.navbar_item}>
+                        <a href="#">PRICING</a>
+                     </li>
+                     <li className={styles.navbar_item}>
+                        <a href="#">CONTACT US</a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
          </div>
       </>
    );
