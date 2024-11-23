1938ddf051582d4155476ea1e4ef0862d6cf4eb5
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-23 12:52:21 +0800

    new navbar

--- src/assets/components/Navbar/Navbar.tsx
+++ src/assets/components/Navbar/Navbar.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import styles from './Navbar.module.scss';
+import React from "react";
+import styles from "./Navbar.module.scss";
 
 interface NavbarProps {
-  brandName: string;
+   brandName: string;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ brandName }) => {
-  return (
-    <nav className={styles.navbar}>
-      <div className={styles.brand}>
-        <h1>{brandName}</h1>
-      </div>
-      <ul className={styles.navList}>
-        <li className={styles.navItem}>Home</li>
-        <li className={styles.navItem}>About</li>
-        <li className={styles.navItem}>Services</li>
-        <li className={styles.navItem}>Contact</li>
-      </ul>
-    </nav>
-  );
+   return (
+      <>
+         <div className={styles.navbar}>
+            <div className={styles.navbar__brand}>{brandName}</div>
+         </div>
+      </>
+   );
 };
 
 export default Navbar;
