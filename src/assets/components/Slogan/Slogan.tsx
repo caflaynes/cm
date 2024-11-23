92e6c1f5b9fea050506f31fe302932d918397106
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-24 01:28:04 +0800

    commit mx container

--- src/assets/components/Slogan/Slogan.tsx
+++ src/assets/components/Slogan/Slogan.tsx
@@ -9,7 +9,7 @@ const Slogan: React.FC<SloganProps> = ({ slogan }) => {
    return (
       <div className={styles.slogan}>
          <div className={styles.slogan__wrapper}>
-            <p className={styles.sloga__text}>{slogan}</p>
+            <p className={styles.slogan__text}>{slogan}</p>
          </div>
       </div>
    );
