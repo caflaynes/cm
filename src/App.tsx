14509fcb706f3fdccd289c070d1a3d2b0cc1e171
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-23 11:51:21 +0800

    test navbar with ts file path

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import { Navbar } from './assets/components';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar brandName="My Website" />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
