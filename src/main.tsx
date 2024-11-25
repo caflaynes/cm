4026c7f8352413f31379b47b655ff10114d8371b
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-25 10:54:46 +0800

    smoothscroll.polyfill

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import smoothscroll from 'smoothscroll-polyfill';
 import './main.scss'
 import App from './App.tsx'
+
+smoothscroll.polyfill();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
